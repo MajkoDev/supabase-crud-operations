1fde46d141cb9e24b5dab7892221afd698b83d32
Marian <hellomariandev@gmail.com>
2023-10-13 00:22:09 +0200

    fetching data from supabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
 import Card from "./components/Card";
 import Form from "./components/Form";
+import { supabase } from "./lib/supabaseClient";
 
 function App() {
+  //TODO: FETCHING DATA from SUPABASE.
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  async function getPosts() {
+    try {
+      // asking for all ('*') data from 'post' table
+      const { data, error } = await supabase
+        .from("posts")
+        .select("*")
+        .limit(10);
+
+      if (error) throw error;
+      if (data != null) {
+        setPosts(data);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="container">
