24c13e1a9578d7a76cea03e9a2c787605215bf81
Marian <hellomariandev@gmail.com>
2023-10-12 23:46:50 +0200

    Update Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -15,7 +15,7 @@ const Form = () => {
         name="title"
         id="title"
         placeholder="Title"
-        className="border bolder-black p-1  rounded-lg" 
+        className="border bolder-black p-1 rounded-lg"
         onChange={(e) => setTitle(e.target.value)}
       />
 
@@ -26,7 +26,7 @@ const Form = () => {
         cols="30"
         rows="10"
         placeholder="Content"
-        className="border bolder-black p-1  rounded-lg"
+        className="border bolder-black p-1 rounded-lg"
         onChange={(e) => setContent(e.target.value)}
       />
       <button className="py-1 px-3 m-1 rounded-lg bg-gray-400 hover:bg-gray-600 text-white">
