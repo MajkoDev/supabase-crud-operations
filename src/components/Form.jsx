5499e83d899f7d082a78717cc28f460c3019d249
Marian <hellomariandev@gmail.com>
2023-10-16 23:44:43 +0200

    create operation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { supabase } from "../lib/supabaseClient";
 
-const Form = ({ setNewTitle, setNewContent }) => {
+const Form = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  async function createPost() {
+    try {
+      // asking for all ('*') data from 'post' table
+      const { data, error } = await supabase
+        .from("posts")
+        .insert({ title: title, content: content });
+      //        .single();
+
+      if (error) throw error;
+      window.location.reload();
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
   return (
     <div className="flex flex-col gap-2 items-center ">
       <label htmlFor="">Title</label>
@@ -10,9 +28,8 @@ const Form = ({ setNewTitle, setNewContent }) => {
         id="title"
         placeholder="Title"
         className="border bolder-black p-1 rounded-lg"
-        onChange={(e) => setNewTitle(e.target.value)}
+        onChange={(e) => setTitle(e.target.value)}
       />
-
       <label htmlFor="">Content</label>
       <textarea
         name="content"
@@ -21,9 +38,12 @@ const Form = ({ setNewTitle, setNewContent }) => {
         rows="10"
         placeholder="Content"
         className="border bolder-black p-1 rounded-lg"
-        onChange={(e) => setNewContent(e.target.value)}
+        onChange={(e) => setContent(e.target.value)}
       />
-      <button className="py-1 px-3 m-1 rounded-lg bg-gray-400 hover:bg-gray-600 text-white">
+      <button
+        onClick={() => createPost()}
+        className="py-1 px-3 m-1 rounded-lg bg-gray-400 hover:bg-gray-600 text-white"
+      >
         Create
       </button>
     </div>
